41f0b69ac9f560cbec11c5d2640d0a4e540997ab
TheCodCat <166803529+TheCodCat@users.noreply.github.com>
2026-01-12 16:30:35 +0400

    asd

---
+++ WarehouseManagementSystemClient/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarehouseManagementSystemClient.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private readonly Shipment.ShipmentClient _client;
+        public MainViewModel()
+        {
+            var channel = GrpcChannel.ForAddress("http://localhost:5103");
+            _client = new Shipment.ShipmentClient(channel);
+
+            //Task.Run(() => TestCheck());
+        }
+
+        public async void TestCheck()
+        {
+            var result = await _client.SetShipmentAsync(new ShipmentRequest()
+            {
+                Partys =
+                {
+                    new Party(){ Product = new Product(){ Id = 0, Name = "asdasd", Description = "asdwrw" }, Count = 1 }
+                }
+            });
+        }
+    }
+}
