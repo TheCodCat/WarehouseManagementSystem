1daf9eea59bf9748668cf16a1cc9f232c1a8998f
TheCodCat <166803529+TheCodCat@users.noreply.github.com>
2026-01-12 16:16:53 +0400

    asd

---
+++ WarehouseManagementSystemClient/Platforms/Android/MainActivity.cs
@@ -0,0 +1,11 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace WarehouseManagementSystemClient
+{
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+    }
+}
